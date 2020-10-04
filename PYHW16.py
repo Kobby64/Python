3e3df4a38c9a6d557654cda6489fcdbf59afa76c
Kobby64 <72305112+Kobby64@users.noreply.github.com>
2020-10-03 22:40:59 -0400

    Update PYHW16.py

--- PYHW16.py
+++ PYHW16.py
@@ -43,3 +43,52 @@ def base10Logarithm(x):
 
 def base2Logarith(x, y):
  return np.log2(x)
+
+# Printing some format
+print("what format you expect the expression to be entered in")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+print("5.Natural logarithm")
+print("6.Base10 logarithm")
+print("7.Base2 logarithm")
+
+choice = input("Enter your preferred choice(1/2/3/4/5/6/7): ")
+# taking user input using num1
+num1 = int(input("Enter first Number: "))  
+ # taking user input using num2
+num2 = int(input("Enter Second Number: "))  
+
+
+#try block
+try:
+    if choice == '1':
+        print(num1, "+", num2, "=", add(num1, num2))
+
+    elif choice == '2':
+        print(num1, "-", num2, "=", subtract(num1, num2))
+
+    elif choice == '3':
+        print(num1, "*", num2, "=", multiply(num1, num2))
+
+    elif choice == '4':
+        print(num1, "/", num2, "=", divide(num1, num2))
+
+    elif choice == '5':
+        print("Natural log of", num1, "=", naturalLogarithm(num1))
+        print("Natural log", num2, "=", naturalLogarithm(num2))
+
+    elif choice == '6':
+        print("Base10 log of", num1, "=", base10Logarithm(num1))
+        print("Base10 log of", num2, "=", base10Logarithm(num2))
+
+    elif choice == '7':
+        print("Base2 log of", num1, "=", base2Logarith(num1))
+        print("Base2 log of", num2, "=", base2Logarith(num2))
+
+    else:
+        print("There is some invalid input")
+        #Exception block executes if any kind of exception program find
+except Exception:
+    print("There is some exception....\nplease try to fix as soon as possible.")
